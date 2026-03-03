d4ffa96b7cb0114163c3076d1ba16a427bbfff21
Rani Arji <raniarji3@gmail.com>
2026-03-03 09:02:19 +0530

    Update face_server.py

--- face_server.py
+++ face_server.py
@@ -71,8 +71,7 @@ def recognize_face():
 def home():
     return "Face Server Running 🚀"
 
-import os
-
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
