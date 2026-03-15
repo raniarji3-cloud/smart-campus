3f7ed11c9d46bb4d07493f45e690cc5e8b6b3065
Rani Arji <raniarji3@gmail.com>
2026-03-15 21:03:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,10 +17,12 @@ app.use(session({
   saveUninitialized: false,
   cookie: {
     httpOnly: true,
-    secure: false, // keep false for localhost
-    maxAge: 1000 * 60 * 60 // 1 hour
+    secure: false,
+    sameSite: "lax",
+    maxAge: 1000 * 60 * 60
   }
 }));
+
 app.get("/test-route", (req, res) => {
   res.send("Route Working");
 });
@@ -33,7 +35,7 @@ app.get("/", (req, res) => {
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-const mysql = require("mysql2");
+
 
 const db = mysql.createConnection({
   host: process.env.MYSQLHOST,
@@ -400,8 +402,7 @@ app.post("/register-face", async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 3000;
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
